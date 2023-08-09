f59b6f238a8ccf061a0689fc73957b21215277c7
Garcat <gar@yeah.net>
2023-08-09 20:50:47 +1000

    Restore getStaticProps

--- src/application/HomePage/getStaticProps.ts
+++ src/application/HomePage/getStaticProps.ts
@@ -1,42 +1,35 @@
 import { GetStaticProps } from 'next'
-// import { BLOGS_PER_PAGE } from './components/ExploreBlogs/ExploreBlogs'
-// import { TagsPerPage } from './components/Tags'
+import { BLOGS_PER_PAGE } from './components/ExploreBlogs/ExploreBlogs'
+import { TagsPerPage } from './components/Tags'
 
-const getStaticProps: GetStaticProps = async () => ({
-  props: {
-    fallback: 'blocking',
-  },
-})
+const getStaticProps: GetStaticProps = async () => {
+  const getTags = (await import('@/services/getTags')).default
+  const getBlogs = (await import('@/services/getBlogs')).default
 
-// {
-//   // const getTags = (await import('@/services/getTags')).default
-//   // const getBlogs = (await import('@/services/getBlogs')).default
+  const tagsOnLargeScreen = await getTags({
+    page: 1,
+    perPage: TagsPerPage.Large,
+  })
 
-//   // const tagsOnLargeScreen = await getTags({
-//   //   page: 1,
-//   //   perPage: TagsPerPage.Large,
-//   // })
+  const tagsOnSmallScreen = await getTags({
+    page: 1,
+    perPage: TagsPerPage.Small,
+  })
 
-//   // const tagsOnSmallScreen = await getTags({
-//   //   page: 1,
-//   //   perPage: TagsPerPage.Small,
-//   // })
+  const blogs = await getBlogs({
+    page: 1,
+    perPage: BLOGS_PER_PAGE,
+  })
 
-//   // const blogs = await getBlogs({
-//   //   page: 1,
-//   //   perPage: BLOGS_PER_PAGE,
-//   // })
-
-//   return {
-//     props: {
-//       // fallback: {
-//       //   [`/api/tags?perPage=${TagsPerPage.Large}`]: tagsOnLargeScreen,
-//       //   [`/api/tags?perPage=${TagsPerPage.Small}`]: tagsOnSmallScreen,
-//       //   [`/api/blogs?perPage=${BLOGS_PER_PAGE}`]: blogs,
-//       // },
-//       fallback: 'blocking',
-//     },
-//   }
-// }
+  return {
+    props: {
+      fallback: {
+        [`/api/tags?perPage=${TagsPerPage.Large}`]: tagsOnLargeScreen,
+        [`/api/tags?perPage=${TagsPerPage.Small}`]: tagsOnSmallScreen,
+        [`/api/blogs?perPage=${BLOGS_PER_PAGE}`]: blogs,
+      },
+    },
+  }
+}
 
 export default getStaticProps
